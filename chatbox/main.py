6408888c3733e12d853a76fa05da002120140f58
chantroi <tonglanhchua@gmail.com>
2024-02-28 10:25:42 +0700

    update chatbox/main.py

--- chatbox/main.py
+++ chatbox/main.py
@@ -1,11 +1,27 @@
 import streamlit as st
-from util import session_state, reload_messages, prompt
 from ws import ws, websocket
 from threading import Thread
 
 Thread(target=websocket, args=(st,)).start()
 
 if name := st.text_input("Hãy nhập tên của bạn"):
-    session_state(st)
-    reload_messages(st)
-    prompt(name, st, ws)
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = []
+    
+    for m in st.session_state.messages:
+        user = st.chat_message(m["user"])
+        if m["type"] == "text":
+            user.write(m["content"])
+        elif m["type"] == "video":
+            user.video(m["content"], height=60)
+        elif m["type"] ==  "image":
+            user.image(m["content"], height=60)
+        elif m["type"] == "audio":
+            user.audio(m["content"])
+           
+    if text := st.chat_input("Write"):
+        msg = {"user": name, "content": text}
+        st.session_state.messages.append(msg)
+        ws.send(str(msg))
+        with st.chat_message(name):
+            st.write(text)
