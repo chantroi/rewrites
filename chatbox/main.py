d85c75cbc3da433fb5c3a2c08600e3ddaf2f2022
chantroi <tonglanhchua@gmail.com>
2024-02-28 09:58:52 +0700

    update chatbox/main.py

--- chatbox/main.py
+++ chatbox/main.py
@@ -3,7 +3,7 @@ from util import session_state, reload_messages, prompt
 from ws import ws, websocket
 from threading import Thread
 
-Thread(target=websocket, args=(st,))
+Thread(target=websocket, args=(st,)).start()
 
 if name := st.text_input("Hãy nhập tên của bạn"):
     session_state(st)
