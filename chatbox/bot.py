7661ed6095e40b601eb2d2ddd0368127daff59da
chantroi <tonglanhchua@gmail.com>
2024-02-28 09:21:52 +0700

    update chatbox/bot.py

--- chatbox/bot.py
+++ chatbox/bot.py
@@ -1,4 +1,5 @@
 from hydrogram import Client
+from util import handle_text, handle_video, handle_image, handle_audio
 import requests
 import os
 
@@ -10,3 +11,21 @@ bot_token = r["bot"]["ai_tg"]
 class Bot:
     def __init__(self):
         self.bot = Client("bot", api_id, api_hash, bot_token=bot_token)
+    
+    def commands(self):
+        @self.bot.on_message()
+        def on_message(c, m):
+            if m.text:
+                handle_text(m)
+            elif m.video:
+                handle_video(m)
+            elif m.photo:
+                handle_image(m)
+            elif m.audio:
+                handle_audio(m)
+            elif m.sticker:
+                handle_image(m)
+                
+    def run(self):
+        self.commands()
+        self.bot.run()
