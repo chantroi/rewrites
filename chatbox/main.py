f1f6fce492015a031d0312b490967be74fac9688
chantroi <tonglanhchua@gmail.com>
2024-02-28 09:24:54 +0700

    update chatbox/main.py

--- chatbox/main.py
+++ chatbox/main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from util import session_state, reload_messages, prompt
+from bot import Bot
+
+bot = Bot()
+bot.run()
+bot = bot.instance()
 
 if name := st.text_input("Hãy nhập tên của bạn"):
     session_state(st)
