cfbbfb7e4b0175eff21e23acff79cf0fcc2b0cb1
chantroi <tonglanhchua@gmail.com>
2024-02-28 10:02:46 +0700

    update chatbox/ws.py

--- chatbox/ws.py
+++ chatbox/ws.py
@@ -6,7 +6,6 @@ ws = connect("ws://localhost:8000")
 
 def websocket(st):
     with ws:
-        ws.send("Connected")
         while True:
             message = ws.recv()
             m = json.loads(message)
