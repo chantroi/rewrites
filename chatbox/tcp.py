123c72d1fa5e6e1c1e9dbec5d58b8156df9ad206
chantroi <tonglanhchua@gmail.com>
2024-02-28 11:35:26 +0700

    update chatbox/tcp.py

--- chatbox/tcp.py
+++ chatbox/tcp.py
@@ -6,7 +6,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def tcp(st):
     client.connect(("127.0.0.1", 8000))
     while True:
-        data_bytes = client.recv(1024)
+        data_bytes = client.recv(1024*1024*1024*1024)
         data_text = data_bytes.decode('utf-8', errors='replace')
         m = json.loads(data_text)
         st.session_state.messages.append(m)
