3a859c24283c7a3cdaf5dd3051890107fdc6b6b1
chantroi <tonglanhchua@gmail.com>
2024-03-01 15:10:07 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse, RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from rmq import Consumer, publish
 from threading import Thread
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"]
+)
 consumer = Consumer()
 Thread(target=consumer.run).start()
 
