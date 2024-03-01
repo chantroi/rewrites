dc389b0ced268968e3d5d290f1849d90e5cebbde
chantroi <tonglanhchua@gmail.com>
2024-03-01 11:12:00 +0700

    update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,16 @@
 from flask import Flask, Response, redirect, request
-from rmq import MQ
-from threading import Thread
+from rMQ import MQ
 
 app = Flask(__name__)
-mq = MQ()
-#Thread(target=mq.run).start()
-mq.run()
+MQ.start()
 
 @app.route("/")
 def home():
-    return Response(mq.get(), mimetype='text/plain')
+    return Response(MQ.get(), mimetype='text/plain')
 
 @app.route("/producer", methods=["GET", "POST"])
 def producer():
     data = request.args.get('data') or request.form.get('data')
     if data:
-        mq.send(data)
+        MQ.send(data)
     return redirect("/")
