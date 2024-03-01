bc66d53e4b6a8f8688a71ef4cd94e5403902f13c
chantroi <tonglanhchua@gmail.com>
2024-03-01 07:47:47 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, RedirectResponse
 from queue import MQ
 
 app = FastAPI()
@@ -9,3 +9,8 @@ mq.run()
 @app.get("/")
 async def home():
     return StreamingResponse(mq.get(), mimetype='text/plain')
+    
+@app.route("/producer", methods=["GET", "POST"])
+async def producer(data: str):
+    mq.send(data)
+    return RedirectResponse("/")
