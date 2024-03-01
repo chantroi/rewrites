f3e77f1dbfd51e3eda62c645c6351f2542f0970d
chantroi <tonglanhchua@gmail.com>
2024-03-01 19:03:14 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-#python -m gunicorn --reload -b 0.0.0.0:10808 main:wsgi_app
-python -m hypercorn main:asgi_app --bind '0.0.0.0:10808' --reload
+python -m gunicorn --reload -b 0.0.0.0:10808 main:wsgi_app
+#python -m hypercorn main:asgi_app --bind '0.0.0.0:10808' --reload
