e50008771c942023819c6959f71fe75c2e7f9985
chantroi <tonglanhchua@gmail.com>
2024-03-01 12:16:57 +0700

    update test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from threading import Thread
 consumer = Consumer()
 deliver = Deliver()
 Thread(target=consumer.run).start()
-Thread(target=deliver.run).start()
 
 def get():
     for i in consumer.get():
