f54375a1f7e6a0857fd668103ddaf3e7aa3acec6
chantroi <tonglanhchua@gmail.com>
2024-03-01 14:45:17 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-python -m gunicorn -b 0.0.0.0:10808 wsgi:app
+python -m gunicorn --reload -b 0.0.0.0:10808 wsgi:app
 #python -m uvicorn main:app --host 0.0.0.0 --port 10808 --reload
